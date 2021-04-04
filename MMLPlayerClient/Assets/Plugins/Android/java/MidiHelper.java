b0f29604f96126f94ad30d3102a4f9634c99ee32
ccggzz123@aliyun.com <CodeGize123>
2021-04-04 16:19:35 +0800

    规范文件位置

--- MMLPlayerClient/Assets/Plugins/Android/java/MidiHelper.java
+++ MMLPlayerClient/Assets/Plugins/Android/java/MidiHelper.java
@@ -1,6 +1,5 @@
 package com.codegize.midihelper;
 
-
 import android.media.midi.*;
 import android.util.Log;
 import java.io.IOException;
@@ -11,19 +10,7 @@ public class MidiHelper {
 
     private static String TAG = "MidiHelper";
 
-    //private static MidiManager mMidiManager;
     private static MidiDriver midiDriver;
-
-    //public static void Init(MidiManager mgr) {
-    //    mMidiManager = mgr;
-    //    if (mMidiManager == null)
-    //        Log.e(TAG, "Init faild: Context.MIDI_SERVICE");
-    //    else
-    //    {
-    //        Log.i(TAG, "Init: success");
-    //        midiDriver = new MidiDriver();
-    //   }
-    //}
 
     public static void OpenDevice()
     {
@@ -32,17 +19,8 @@ public class MidiHelper {
 			midiDriver = new MidiDriver();
 		}
         midiDriver.start();
-        // Get the configuration.
         int[] config = midiDriver.config();
-
-        Log.i(TAG, "OpenDevice:");
-        // Print out the details.
-        Log.d(TAG, "maxVoices: " + config[0]);
-        Log.d(TAG, "numChannels: " + config[1]);
-        Log.d(TAG, "sampleRate: " + config[2]);
-        Log.d(TAG, "mixBufferSize: " + config[3]);
     }
-
 
     public static void CloseDevice()
     {
